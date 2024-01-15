48e9d993cea221776fa2419115f5a7f31c5f15a3
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-01-15 16:51:58 +0100

    Fix schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE `secure_login`;
-
+CREATE DATABASE secure_login;
+USE secure_login;
 CREATE USER 'sec_user'@'localhost' IDENTIFIED BY 'eKcGZr59zAa2BEWU';
-GRANT SELECT, INSERT, UPDATE ON `secure_login`.* TO 'sec_user'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON secure_login.* TO 'sec_user'@'localhost';
 
-CREATE TABLE `secure_login`.`members` (
+CREATE TABLE members (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
   `username` VARCHAR(30) NOT NULL, 
   `email` VARCHAR(50) NOT NULL, 
@@ -11,10 +11,10 @@ CREATE TABLE `secure_login`.`members` (
   `salt` CHAR(128) NOT NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE `secure_login`.`login_attempts` (
+CREATE TABLE `login_attempts` (
   `user_id` INT(11) NOT NULL,
   `time` VARCHAR(30) NOT NULL 
-) ENGINE=InnoDB
+) ENGINE=InnoDB;
 
-INSERT INTO `secure_login`.`members` VALUES(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
+INSERT INTO members VALUES(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
 
