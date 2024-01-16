7da44c010030fde46e93a426b649f4c0546a099e
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-01-16 11:22:07 +0100

    Make username and email unique fields

--- schema.sql
+++ schema.sql
@@ -5,9 +5,9 @@ GRANT SELECT, INSERT, UPDATE ON secure_login.* TO 'sec_user'@'localhost';
 
 CREATE TABLE members (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  `username` VARCHAR(30) NOT NULL, 
-  `email` VARCHAR(50) NOT NULL, 
-  `password` CHAR(128) NOT NULL, 
+  `username` VARCHAR(30) NOT NULL UNIQUE, 
+  `email` VARCHAR(50) NOT NULL UNIQUE, 
+  `password` CHAR(128) NOT NULL,
   `salt` CHAR(128) NOT NULL
 ) ENGINE = InnoDB;
 
