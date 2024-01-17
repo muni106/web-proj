7dfb3f8c2838c841467443ab28a40dfa102e7ccb
Michele Ceccacci <michelececcacci1@gmail.com>
2024-01-17 15:35:27 +0100

    introduce schema

--- schema.sql
+++ schema.sql
@@ -18,3 +18,19 @@ CREATE TABLE `login_attempts` (
 
 INSERT INTO members VALUES(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
 
+CREATE TABLE posts (
+  `post` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` VARCHAR(70) NOT NULL,
+  `body` VARCHAR(2000),
+  `reply` INT
+) ENGINE = InnoDB;
+
+CREATE TABLE likes (
+  `user_id` INT NOT NULL,
+  `post_id` INT NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE followers (
+  `follower_id` INT NOT NULL,
+  `followee_id` INT NOT NULL
+) ENGINE = InnoDB;
