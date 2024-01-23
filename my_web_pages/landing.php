3b52a777e53441b8c6a2f297d1bd0a163f0286d2
Michele Ceccacci <michelececcacci1@gmail.com>
2024-01-23 13:59:45 +0100

    introduce landing page

---
+++ my_web_pages/landing.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Welcome to SocialName</title>
+</head>
+<body>
+    Welcome to SocialName
+    Join now
+    <a href="signup.php">  
+        <input type="button" value="Sign up" onclick="" class="btn bg-black block-btn text-white fw-bold rounded-pill d-block mx-3" alt="sign up" height="5%" width="5%"> 
+    </a>
+    <a href="login.php">  
+        <input type="button" value="Login" onclick="" class="btn bg-black block-btn text-white fw-bold rounded-pill d-block mx-3" alt="login" height="5%" width="5%"> 
+    </a>
+        
+</body>
+</html>
