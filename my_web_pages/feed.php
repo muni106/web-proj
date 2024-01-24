40de4c2f9eb5c254c652a95ac63e4f3cf8ca2c33
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-01-24 11:39:46 +0100

    Make text wrap in feed

--- my_web_pages/feed.php
+++ my_web_pages/feed.php
@@ -19,7 +19,7 @@
     sec_session_start();
     if (login_check($mysqli)):
     ?>
-        <main class="container-fluid p-2 mt-5 bg-white">
+        <main class="container p-2 mt-5 bg-white">
         <h1 class="fw-bolder border-bottom py-3">Posts</h1>
     <?php
         require_once("get_feed.php");
@@ -27,15 +27,15 @@
         foreach($posts as $post):
     ?>
 
-        <article class="row border-bottom py-3">
+        <article class="row border-bottom py-3 w-100">
             <div class="col-2 text-center">PH</div>
             <section class="col-10 d-grid gap-2">
                 <a rel="author" href="" class="text-decoration-none text-reset fw-bold"><?php echo $post["author"]; ?></a>
-                <p class="text-wrap">
+                <p class="text-wrap text-truncate">
                     <?php echo $post["body"]; ?>
                     <!-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium quam architecto quaerat amet obcaecati voluptatem nobis soluta magni libero ea nostrum eaque ducimus, distinctio natus adipisci modi neque blanditiis id! -->
                 </p>
-                <code class="d-block p-2 text-wrap">
+                <code class="d-block p-2 text-wrap text-truncate">
                     <?php echo $post["code"]; ?>
                     <!-- print("hello world") -->
                     <!-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum deserunt necessitatibus, quasi a quis perspiciatis impedit, consequuntur officiis accusamus, odio minima nesciunt vitae adipisci. Eligendi deserunt sit perspiciatis itaque quam. -->
