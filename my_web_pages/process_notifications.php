ff116a9ac870625d24d37ff66babc5938b08be88
Michele Ceccacci <michelececcacci1@gmail.com>
2024-01-26 23:34:45 +0100

    implement notification date check

--- my_web_pages/process_notifications.php
+++ my_web_pages/process_notifications.php
@@ -2,5 +2,13 @@
 
 require('db_connect.php');
 sec_session_start();
-echo $_SESSION;
+$username = $_SESSION["username"];
+$current_datetime = $_POST["current_datetime"];
+if ($select_stmt = $mysqli->prepare("UPDATE members SET last_notification_check = ? WHERE username = ?")) {
+   $select_stmt->bind_param('ss', $current_datetime, $username);
+   if ($select_stmt->execute()) {
+      echo "Notifications cleared successfully";
+   }
+}
+
 ?>
