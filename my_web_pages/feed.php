574f4dfcd9b3ed6ad91c2e729e1602056c0a1f22
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-02-07 16:06:35 +0100

    Fix navbar heights and extract navbar as compoent

--- my_web_pages/feed.php
+++ my_web_pages/feed.php
@@ -19,47 +19,7 @@
     <?php
     if (login_check($mysqli)) :
     ?>
-        <aside id="menu_desktop">
-            <img src="assets/images/logo.png" alt="logo">
-            <ul>
-                <li>
-                    <a href="feed.php">
-                        <img src="./assets/images/Home.png" alt="homeImg">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src="./assets/images/Explore.png" alt="explore">
-                        Explore
-                    </a>
-                </li>
-                <li>
-                    <a href="saved_posts.php">
-                        <img src="./assets/images/Saved.png" alt="savedPosts">
-                        Saved
-                    </a>
-                </li>
-                <li>
-                    <a href="notifications.php">
-                        <img src="./assets/images/Notifications.png" alt="notifications">
-                        Notifications
-                    </a>
-                </li>
-                <li>
-                    <a href="write_post.php">
-                        <img src="./assets/images/Write.png" alt="writePost">
-                        Write
-                    </a>
-                </li>
-                <li>
-                    <a href="userProfileMe.php">
-                        <img src="./assets/images/Profile.png" alt="profilePersonal">
-                        Profile
-                    </a>
-                </li>
-            </ul>
-        </aside>
+        <?php require("navbar.php") ?>
         <main class="container">
             <h1 class="fw-bolder border-bottom py-3">Posts</h1>
             <?php
---
+++ my_web_pages/navbar.php
@@ -0,0 +1,41 @@
+<nav id="menu_desktop">
+    <img src="assets/images/logo.png" alt="logo">
+    <ul>
+        <li>
+            <a href="feed.php">
+                <img src="./assets/images/Home.png" alt="homeImg">
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <img src="./assets/images/Explore.png" alt="explore">
+                Explore
+            </a>
+        </li>
+        <li>
+            <a href="saved_posts.php">
+                <img src="./assets/images/Saved.png" alt="savedPosts">
+                Saved
+            </a>
+        </li>
+        <li>
+            <a href="notifications.php">
+                <img src="./assets/images/Notifications.png" alt="notifications">
+                Notifications
+            </a>
+        </li>
+        <li>
+            <a href="write_post.php">
+                <img src="./assets/images/Write.png" alt="writePost">
+                Write
+            </a>
+        </li>
+        <li>
+            <a href="userProfileMe.php">
+                <img src="./assets/images/Profile.png" alt="profilePersonal">
+                Profile
+            </a>
+        </li>
+    </ul>
+</nav>
