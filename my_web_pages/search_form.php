16caef7de44243b3061865f448121098ab678d1c
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-02-07 14:38:31 +0100

    Fix color contrast in search bar

--- my_web_pages/search_form.php
+++ my_web_pages/search_form.php
@@ -1,6 +1,6 @@
 <form action="./show_search_results.php" method="get" id="search-bar">
     <label for="search" id="search-input" class="d-none">Write username to search</label>
-    <input id="search" type="text" placeholder="Someone.." name="word" class="d-inline" />
+    <input id="search" type="text" placeholder="Someone.." name="word" class="d-inline text-dark" />
     <label for="search-button" class="d-none">Search</label>
     <input type="image" id="search-button" src="assets/images/Explore.png" alt="Submit"/>
     <!-- <input type="image" src="assets/images/Explore.png" alt="Submit"/> -->
