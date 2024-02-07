5f0476042cb9f909694282eaeb7b756f13b482be
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-02-07 14:36:50 +0100

    Fix followers and follwing html pages

--- my_web_pages/followers.php
+++ my_web_pages/followers.php
@@ -1,11 +1,25 @@
-<title>
-    Followers
-</title>
-<?php
-require_once("db_info.php");
-require_once("get_user_info.php");
-$user_id = $_GET["user_id"];
-$following = get_followers($user_id);
-require_once("users_list.php");
-show_users($following);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Followers
+    </title>
+    <link rel="shortcut icon" type="image/png" href="assets/images/logo.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/chrome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+</head>
+<body>
+    <?php
+    require_once("db_info.php");
+    require_once("get_user_info.php");
+    $user_id = $_GET["user_id"];
+    $following = get_followers($user_id);
+    require_once("users_list.php");
+    show_users($following);
+    ?>   
+</body>
+</html>
--- my_web_pages/following.php
+++ my_web_pages/following.php
@@ -1,11 +1,18 @@
-<title>
-    Following
-</title>
-<?php
-require_once("db_info.php");
-require_once("get_user_info.php");
-$user_id = $_GET["user_id"];
-$following = get_following($user_id);
-require_once("users_list.php");
-show_users($following);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Following</title>
+</head>
+<body>
+    <?php
+    require_once("db_info.php");
+    require_once("get_user_info.php");
+    $user_id = $_GET["user_id"];
+    $following = get_following($user_id);
+    require_once("users_list.php");
+    show_users($following);
+    ?>
+</body>
+</html>
